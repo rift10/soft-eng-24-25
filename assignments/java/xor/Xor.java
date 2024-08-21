b8c68b94527096818299582712557496da5f9777
Rebecca <119640842+rift10@users.noreply.github.com>
2024-08-21 15:15:27 -0700

    Updating assignments/java/xor/Xor.java

--- assignments/java/xor/Xor.java
+++ assignments/java/xor/Xor.java
@@ -67,7 +67,7 @@ public class Xor {
   public byte[] xorByteArray(byte[] array, int length) {
     byte[] result = new byte[length];
     for (int i = 0; i < array.length; i++) {
-      result[i] = xor(array[i], getBit(key, i+2));
+      result[i] = xor(array[i], getBit(key, i+3));
     }
     return result;
   }
