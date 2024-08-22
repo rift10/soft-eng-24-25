e6dd0cc7faf29955af3a1524c1b02fd97e36609b
Rebecca <119640842+rift10@users.noreply.github.com>
2024-08-22 10:32:11 -0700

    Updating assignments/java/xor/Xor.java

--- assignments/java/xor/Xor.java
+++ assignments/java/xor/Xor.java
@@ -47,7 +47,7 @@ public class Xor {
   public byte[] cipherToBytes(String cipher) {
     byte[] cipherBytes = new byte[cipher.length() / 2];
     int index = 0;
-    for (int i = 0; i < cipher.length(); i += 2) {
+    for (int i = 0; i < cipher.length() - 2; i += 2) {
       cipherBytes[i] = (byte) Integer.parseInt(cipher.substring(i, i+2), 16);
       index++;
     }
