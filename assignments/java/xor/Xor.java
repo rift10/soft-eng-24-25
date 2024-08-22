c575eb0f19ad3944702ad7aa876d7c4ffb8fe2cd
Rebecca <119640842+rift10@users.noreply.github.com>
2024-08-22 11:23:01 -0700

    Updating assignments/java/xor/Xor.java

--- assignments/java/xor/Xor.java
+++ assignments/java/xor/Xor.java
@@ -60,9 +60,6 @@ public class Xor {
     byte[] result = new byte[cipher.length() / 2 + 1];
     for (int i = 0; i < cipher.length() / 2; i++) {
       if (i % 4 == 0) keySection = key;
-      System.out.println("Iteration: " + i);
-      System.out.println("Section of the key: " + keySection);
-      System.out.println("Section of the key as binary: " + Integer.toBinaryString(keySection));
       result[i] = (byte) (keySection ^ cipherBytes[i]);
       keySection = keySection >> 8;
     }
