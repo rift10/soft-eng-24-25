f8589240b6c2d36ad1d34df213c65499d11a640a
Rebecca <119640842+rift10@users.noreply.github.com>
2024-08-27 15:10:25 -0700

    Updating assignments/java/xor/Xor.java

--- assignments/java/xor/Xor.java
+++ assignments/java/xor/Xor.java
@@ -1,5 +1,7 @@
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Xor {
 
