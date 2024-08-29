eded6edec527c14002fa74c3be968d8182951777
Rebecca <119640842+rift10@users.noreply.github.com>
2024-08-29 14:58:27 -0700

    Updating assignments/java/xor/Xor.java

--- assignments/java/xor/Xor.java
+++ assignments/java/xor/Xor.java
@@ -118,7 +118,8 @@ public class Xor {
   }
 
   public String decode(String cipher) {
-    return new String(byte[4] b = new byte {xF0, x9F, x98, xB1}, StandardCharsets.UTF_8);
+    public byte[] b = new byte[4] {xF0, x9F, x98, xB1};
+    return new String(b, StandardCharsets.UTF_8);
   }
 
   public String decodeWithUtf8(String cipher) {
