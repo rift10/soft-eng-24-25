1299b263fa678a7040af05994a4fefb3d4e2df9f
Rebecca <119640842+rift10@users.noreply.github.com>
2024-08-30 06:26:41 -0700

    Updating assignments/java/xor/Xor.java

--- assignments/java/xor/Xor.java
+++ assignments/java/xor/Xor.java
@@ -118,6 +118,6 @@ public class Xor {
 
   public static void main(String[] argv) throws Exception {
     // System.out.println(new Xor(567231495).decode(CIPHERTEXT));
-    System.out.println(new Xor(567231495).decodeWithUtf8(text));
+    System.out.println(new Xor(567231495).decodeWithUtf8("hello world"));
   }
 }
