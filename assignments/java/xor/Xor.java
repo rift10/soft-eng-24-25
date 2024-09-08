39cdafe69ee1218dd2b726fe9edd894044ce8cf4
Rebecca <119640842+rift10@users.noreply.github.com>
2024-09-07 21:19:48 -0700

    Updating assignments/java/xor/Xor.java

--- assignments/java/xor/Xor.java
+++ assignments/java/xor/Xor.java
@@ -85,7 +85,7 @@ public class Xor {
     System.out.println(letterBytes.length);
     for (int i = 0; i < letterBytes.length; i++) {
       for (int j = 0; j < byteMasks.length; j++)
-        System.out.println("byte: " + Integer.toBinaryString(letterBytes[i]) + ", checker: " + Integer.toBinaryString(byteMasks[j]) + ", result: " +  (letterBytes[i] & (byteMasks[j])) + ", boolean: " + ((letterBytes[i] & (byteMasks[j])) == 0b1000000));
+        // System.out.println("byte: " + Integer.toBinaryString(letterBytes[i]) + ", checker: " + Integer.toBinaryString(byteMasks[j]) + ", result: " +  (letterBytes[i] & (byteMasks[j])) + ", boolean: " + ((letterBytes[i] & (byteMasks[j])) == 0b1000000));
     }
 
     for (int i = 0; i < letterBytes.length - 1; i += numBytes + 1) {
