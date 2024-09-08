52c08a6764b99cafe6c69d9d1cb0ba02503b5cc0
Rebecca <119640842+rift10@users.noreply.github.com>
2024-09-08 00:17:54 -0700

    Updating assignments/java/xor/Xor.java

--- assignments/java/xor/Xor.java
+++ assignments/java/xor/Xor.java
@@ -111,7 +111,7 @@ public class Xor {
       }
 
       codePoint = letterBytes[i++] & firstByteMasks[numBytes - 1];
-      for (int j = 1; j < numBytes - 1; j++) {
+      for (int j = 0; j < numBytes - 1; j++) {
         codePoint = (codePoint << 6 | (letterBytes[i + j] & 0b00111111));
       }
       sb.append(Character.toChars(codePoint));
