f6d89a3e4989d619861296785eb7679b502329ea
Rebecca <119640842+rift10@users.noreply.github.com>
2024-09-11 15:32:57 -0700

    key stroke listening (doesnt work)

--- projects/game/Input.java
+++ projects/game/Input.java
@@ -1,12 +1,48 @@
-import java.util.Scanner;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class Input {
+public class Input extends KeyAdapter {
 
-    private Scanner mScanner = new Scanner(System.in);
-    private int currentMove;
+    private enum Direction {
+        NONE,
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
 
-    public Input() {
+    private int mCurrentKeyCode;
+    private char mCurrentKeyChar;
+    private Direction mCurrentDirection = Direction.NONE;
 
+    public Input() {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        mCurrentKeyCode = e.getKeyCode();
+        mCurrentKeyChar = e.getKeyChar();
+
+        if (mCurrentKeyCode == KeyEvent.VK_LEFT) {
+            mCurrentDirection = Direction.LEFT;
+        } else if (mCurrentKeyCode == KeyEvent.VK_RIGHT) {
+            mCurrentDirection = Direction.RIGHT;
+        } else if (mCurrentKeyCode == KeyEvent.VK_UP) {
+            mCurrentDirection = Direction.UP;
+        } else if (mCurrentKeyCode == KeyEvent.VK_DOWN) {
+            mCurrentDirection = Direction.DOWN;
+        } 
+    }
+
+    public int getCurrentKeyCode() {
+        return mCurrentKeyCode;
+    }
+
+    public char getCurrentKey() {
+        return mCurrentKeyChar;
+    }
+
+    public Direction getDirection() {
+        return mCurrentDirection;
     }
 
 }
--- projects/game/Main.java
+++ projects/game/Main.java
@@ -1,5 +1,15 @@
+import javax.swing.JTextArea;
+import javax.swing.text.html.HTMLDocument;
+
 public class Main {
+
+    public static Input mInput = new Input();
+    public static JTextArea mTextArea = new JTextArea(new HTMLDocument(), "hello world", 10, 10);
+
     public static void main(String[] args) {
-        
+        mTextArea.addKeyListener(mInput);
+        // while (true) {
+        //     System.out.println(mInput.getDirection());
+        // }
     }
 }
