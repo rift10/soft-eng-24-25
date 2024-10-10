39ef3cb85726626ae97b54aa1ec431081e9845a9
Rebecca <119640842+rift10@users.noreply.github.com>
2024-10-10 15:23:52 -0700

    Updating assignments/java/streams/Streams.java

--- assignments/java/streams/Streams.java
+++ assignments/java/streams/Streams.java
@@ -87,14 +87,12 @@ public class Streams {
         .toArray(String[]::new);
   }
 
-  // public String[] collectShouting(String[] ss) {
-  //   Object[] result = Arrays
-  //       .stream(ss)
-  //       .map(s -> s.toUpperCase())
-  //       .toArray(s -> toString(s));
-  //   System.err.println(result);
-  //   return new String[3];
-  // }
+  public String[] collectShouting(String[] ss) {
+    return Arrays
+        .stream(ss)
+        .map(s -> s.toUpperCase())
+        .toArray(String[]::new);
+  }
 
   // public int hailstoneLength() {}
 
