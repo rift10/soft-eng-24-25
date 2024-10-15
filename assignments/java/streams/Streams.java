05e24c751c8c757921805376fe1bc02bb9fd887a
Rebecca <119640842+rift10@users.noreply.github.com>
2024-10-14 20:33:01 -0700

    Updating assignments/java/streams/Streams.java

--- assignments/java/streams/Streams.java
+++ assignments/java/streams/Streams.java
@@ -130,7 +130,7 @@ public class Streams {
     return Arrays 
         .stream(ss)
         // .map(s -> s.length())
-        .collect(Collectors.groupingBy(s -> s.length()))
+        .collect(Collectors.groupingBy(s -> s.length()), Collectors.toList())
         .toArray();
   }
 
