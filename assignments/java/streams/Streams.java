13b904b1e738601bb9b5f6f385a8338c879707e6
Rebecca <119640842+rift10@users.noreply.github.com>
2024-10-15 22:44:21 -0700

    Updating assignments/java/streams/Streams.java

--- assignments/java/streams/Streams.java
+++ assignments/java/streams/Streams.java
@@ -129,7 +129,7 @@ public class Streams {
   public int[] lengthHistogram(String[] ss) {
     return Arrays 
         .stream(ss)
-        .flatMapToInt(s -> s.length())
+        .mapToInt(s -> s.length())
         .takeWhile(s -> s == 0)
         .collect(Collectors.summingInt())
         .toArray();
