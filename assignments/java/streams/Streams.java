defacd74b8b2a7524d1364373ac3f812cb618eb5
Rebecca <119640842+rift10@users.noreply.github.com>
2024-10-15 22:45:40 -0700

    Updating assignments/java/streams/Streams.java

--- assignments/java/streams/Streams.java
+++ assignments/java/streams/Streams.java
@@ -131,7 +131,7 @@ public class Streams {
         .stream(ss)
         .mapToInt(s -> s.length())
         .takeWhile(s -> s == 0)
-        .collect(Collectors.summingInt(Integer.valueof()))
+        .collect(Collectors.summingInt(Integer.valueof(s)))
         .toArray();
   }
 
