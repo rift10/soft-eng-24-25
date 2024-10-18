d02e2452d1be4cd2010ddf6bcf65aeede83f93c3
Rebecca <119640842+rift10@users.noreply.github.com>
2024-10-18 21:44:00 +0000

    created album and artist records, with method to check if an album is created by a certain artist

---
+++ projects/records/Records.java
@@ -0,0 +1,52 @@
+package projects.records;
+
+import java.util.ArrayList;
+
+public class Records {
+    
+    public record Album(String name, int numSongs) {
+
+        private boolean isCreatedBy(Artist artist) {
+            if (artist.albums() == null) return false;
+
+            for (String i : artist.albums()) {
+                if (i.equals(this.name())) return true;
+            }
+
+            return false; 
+        }
+    }
+
+    public record Artist(String stageName, String realName, String[] albums, int age) {}
+
+    private static final Album one = new Album("a", 10);
+    private static final Album two = new Album("b", 11);
+    private static final Album three = new Album("c", 12);
+    private static final Album four = new Album("d", 13);
+    private static final Album five = new Album("e", 14);
+
+    private static final Artist artistOne = new Artist("A", "", new String[] {one.name(), two.name(), three.name()}, 0);
+    private static final Artist artistTwo = new Artist("B", "", new String[] {four.name(), five.name()}, 0);
+
+    private static final Album[] allAlbums = new Album[] {one, two, three, four, five};
+    
+    private static final ArrayList<Album> listByOne = new ArrayList<>();
+    private static final ArrayList<Album> listByTwo= new ArrayList<>();
+
+    public static void main(String[] args) {
+        for (Album album : allAlbums) {
+            if (album.isCreatedBy(artistOne)) listByOne.add(album);
+            if (album.isCreatedBy(artistTwo)) listByTwo.add(album);
+        }
+
+        System.out.println("albums by artist one:");
+        for (Album album : listByOne) {
+            System.out.println(album.name());
+        }
+
+        System.out.println("albums by artist two:");
+        for (Album album : listByTwo) {
+            System.out.println(album.name());
+        }
+    }
+}
