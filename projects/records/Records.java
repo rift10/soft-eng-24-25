2c71dc6b2cb870918448fcf7a86ae17e0c72e7db
Rebecca <119640842+rift10@users.noreply.github.com>
2024-10-22 22:29:00 +0000

    added in a method using both records, made getter for lists, added more comments

--- projects/records/Records.java
+++ projects/records/Records.java
@@ -1,6 +1,7 @@
 package projects.records;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Records {
     
@@ -9,16 +10,27 @@ public class Records {
         private boolean isCreatedBy(Artist artist) {
             // to avoid null errors
             if (artist.albums() == null) return false;
-
             for (String i : artist.albums()) {
                 if (i.equals(this.name())) return true;
             }
-
             return false; 
         }
     }
 
-    public record Artist(String stageName, String realName, String[] albums, int age) {}
+    public record Artist(String stageName, String realName, String[] albums, int age) {
+
+        /**
+         * @param minNumSongs the minimum number of songs to return true
+         * @return true if this artist has released an album with at least the specified amount of songs
+         */
+        private boolean hasAlbumWithMinSongs(int minNumSongs) {
+            var albumsByArtist = getListByArtist(this);
+            for (Album album : albumsByArtist) {
+                if (album.numSongs >= minNumSongs) return true;
+            }
+            return false;
+        }
+    }
 
     // generic names for now, might fill out later
     private static final Album one = new Album("a", 10);
@@ -30,17 +42,30 @@ public class Records {
     private static final Artist artistOne = new Artist("A", "", new String[] {one.name(), two.name(), three.name()}, 0);
     private static final Artist artistTwo = new Artist("B", "", new String[] {four.name(), five.name()}, 0);
 
+    // arrays for all albums and artists for looping through
     private static final Album[] allAlbums = new Album[] {one, two, three, four, five};
+    private static final Artist[] allArtists = new Artist[] {artistOne, artistTwo};
     
     // lists to contain albums created by a certain artist
-    private static final ArrayList<Album> listByOne = new ArrayList<>();
-    private static final ArrayList<Album> listByTwo= new ArrayList<>();
+    private static List<Album> listByOne = new ArrayList<>();
+    private static List<Album> listByTwo= new ArrayList<>();
+
+    /**
+     * @param artist the artist to get the albums created by
+     * @return an arraylist of the albums created by the specified artist
+     */
+    private static List<Album> getListByArtist(Artist artist) {
+        var result = new ArrayList<Album>();
+        for (Album album : allAlbums) {
+            if (album.isCreatedBy(artist)) result.add(album);
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        for (Album album : allAlbums) {
-            if (album.isCreatedBy(artistOne)) listByOne.add(album);
-            if (album.isCreatedBy(artistTwo)) listByTwo.add(album);
-        }
+
+        listByOne = getListByArtist(artistOne);
+        listByTwo = getListByArtist(artistTwo);
 
         System.out.println("albums by artist one:");
         for (Album album : listByOne) {
@@ -50,6 +75,12 @@ public class Records {
         System.out.println("albums by artist two:");
         for (Album album : listByTwo) {
             System.out.println(album.name());
+        }
+
+        final int minNumSongs = 13;
+        System.out.println("artists with at least " + minNumSongs + " songs on an album:");
+        for (Artist artist : allArtists) {
+            if (artist.hasAlbumWithMinSongs(minNumSongs)) System.out.println(artist.stageName());
         }
     }
 }
