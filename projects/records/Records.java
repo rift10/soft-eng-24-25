bc9200c66f3e9590f5cafc9430e82dfb7826c5c4
Rebecca <119640842+rift10@users.noreply.github.com>
2024-10-23 22:24:50 +0000

    refactored how the two records are linked to each other, implemented streams, added in another method

--- projects/records/Records.java
+++ projects/records/Records.java
@@ -1,23 +1,13 @@
 package projects.records;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Records {
     
-    public record Album(String name, int numSongs) {
+    public record Album(String name, int numSongs, String artist) {}
 
-        private boolean isCreatedBy(Artist artist) {
-            // to avoid null errors
-            if (artist.albums() == null) return false;
-            for (String i : artist.albums()) {
-                if (i.equals(this.name())) return true;
-            }
-            return false; 
-        }
-    }
-
-    public record Artist(String stageName, String realName, String[] albums, int age) {
+    public record Artist(String stageName, int age) {
 
         /**
          * @param minNumSongs the minimum number of songs to return true
@@ -33,54 +23,71 @@ public class Records {
     }
 
     // generic names for now, might fill out later
-    private static final Album one = new Album("a", 10);
-    private static final Album two = new Album("b", 11);
-    private static final Album three = new Album("c", 12);
-    private static final Album four = new Album("d", 13);
-    private static final Album five = new Album("e", 14);
+    private static final Artist artistOne = new Artist("A", 25);
+    private static final Artist artistTwo = new Artist("B", 35);
 
-    private static final Artist artistOne = new Artist("A", "", new String[] {one.name(), two.name(), three.name()}, 0);
-    private static final Artist artistTwo = new Artist("B", "", new String[] {four.name(), five.name()}, 0);
+    private static final Album one = new Album("a", 10, artistOne.stageName);
+    private static final Album two = new Album("b", 11, artistOne.stageName);
+    private static final Album three = new Album("c", 12, artistOne.stageName);
+    private static final Album four = new Album("d", 13, artistTwo.stageName);
+    private static final Album five = new Album("e", 14, artistTwo.stageName);
 
     // arrays for all albums and artists for looping through
-    private static final Album[] allAlbums = new Album[] {one, two, three, four, five};
     private static final Artist[] allArtists = new Artist[] {artistOne, artistTwo};
+    private static final Album[] allAlbums = new Album[] {one, two, three, four, five};
     
-    // lists to contain albums created by a certain artist
-    private static List<Album> listByOne = new ArrayList<>();
-    private static List<Album> listByTwo= new ArrayList<>();
-
     /**
      * @param artist the artist to get the albums created by
-     * @return an arraylist of the albums created by the specified artist
+     * @return a list of the albums created by the specified artist
      */
     private static List<Album> getListByArtist(Artist artist) {
-        var result = new ArrayList<Album>();
-        for (Album album : allAlbums) {
-            if (album.isCreatedBy(artist)) result.add(album);
-        }
-        return result;
+        return Arrays.stream(allAlbums)
+                .filter(a -> a.artist.equals(artist.stageName))
+                .toList();
+    }
+
+    /**
+     * @param minAge the minimum age the artist has to be
+     * @return a list of artists that are at least the specified number of years old
+     */
+    private static List<Artist> getArtistsWithMinAge(int minAge) {
+        return Arrays.stream(allArtists)
+                .filter(a -> a.age >= minAge)
+                .toList();
+    }
+
+    /**
+     * @param minNumSongs the minimum number of songs that needs to be on an albu
+     * @return a list of the artists that have at least the specified amount of songs
+     */
+    private static List<Artist> getArtistsWithMinSongsOnAlbum(int minNumSongs) {
+        return Arrays.stream(allArtists)
+                .filter(a -> a.hasAlbumWithMinSongs(minNumSongs))
+                .toList();
     }
 
     public static void main(String[] args) {
 
-        listByOne = getListByArtist(artistOne);
-        listByTwo = getListByArtist(artistTwo);
-
         System.out.println("albums by artist one:");
-        for (Album album : listByOne) {
+        for (Album album : getListByArtist(artistOne)) {
             System.out.println(album.name());
         }
 
         System.out.println("albums by artist two:");
-        for (Album album : listByTwo) {
+        for (Album album : getListByArtist(artistTwo)) {
             System.out.println(album.name());
+        }
+
+        final int minAge = 30;
+        System.out.println("artists that are at least " + minAge + " years old:");
+        for (Artist artist : getArtistsWithMinAge(minAge)) {
+            System.out.println(artist.stageName());
         }
 
         final int minNumSongs = 13;
         System.out.println("artists with at least " + minNumSongs + " songs on an album:");
-        for (Artist artist : allArtists) {
-            if (artist.hasAlbumWithMinSongs(minNumSongs)) System.out.println(artist.stageName());
+        for (Artist artist : getArtistsWithMinSongsOnAlbum(minNumSongs)) {
+            System.out.println(artist.stageName());
         }
     }
 }
