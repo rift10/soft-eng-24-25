0ddb07f4105ce85276415f942e2dd612dabf9bfd
Rebecca <119640842+rift10@users.noreply.github.com>
2024-10-31 21:44:23 +0000

    Starting project monte-carlo with starter code from monte-carlo.

---
+++ projects/monte-carlo/RandomVariable.java
@@ -0,0 +1,16 @@
+import java.util.stream.DoubleStream;
+
+public interface RandomVariable {
+
+  /**
+   * Generate the next random value.
+   */
+  public double next();
+
+  /**
+   * Generate a stream of random values.
+   */
+  public default DoubleStream stream() {
+    return DoubleStream.generate(this::next);
+  }
+}
