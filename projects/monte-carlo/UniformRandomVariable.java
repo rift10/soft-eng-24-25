65ea0d08e6ebaf8812ca789222fc17e22d36d843
Rebecca <119640842+rift10@users.noreply.github.com>
2024-11-06 22:41:11 +0000

    random variables

---
+++ projects/monte-carlo/UniformRandomVariable.java
@@ -0,0 +1,18 @@
+import java.util.random.RandomGenerator;
+
+public class UniformRandomVariable implements RandomVariable {
+
+    private final double min, max;
+    private final RandomGenerator r = RandomGenerator.of("L128X1024MixRandom");
+
+    public UniformRandomVariable(double min, double max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public double next() {
+        return r.nextDouble(min, max);
+    }
+    
+}
