8fe39e29c5ea2132578a8d04f444d39f117265c9
Rebecca <119640842+rift10@users.noreply.github.com>
2024-11-07 23:26:07 +0000

    90% intervals

---
+++ projects/monte-carlo/Interval.java
@@ -0,0 +1,44 @@
+
+/**
+ * represents 90% prediction interval
+ */
+public class Interval {
+
+    private final double lowerBound, upperBound;
+
+    public Interval(double min, double max) {
+        this.lowerBound = min;
+        this.upperBound = max;
+    }
+
+    public double getLowerBound() {
+        return lowerBound;
+    }
+
+    public double getUpperBound() {
+        return upperBound;
+    }
+
+    public double getRange() {
+        return upperBound - lowerBound;
+    }
+
+    @Override
+    public String toString() {
+        return "lower bound: "  + lowerBound + ", upper bound: " + upperBound;
+    }
+
+    public static UniformRandomVariable uniformRandomWithInterval(Interval interval) {
+        var x = (interval.getRange() / 0.9 - interval.getRange()) / 2;
+        return new UniformRandomVariable(interval.getLowerBound() - x, interval.getUpperBound() + x);
+    }
+
+    public static NormalRandomVariable normalRandomWithInterval(Interval interval) {
+        return new NormalRandomVariable((interval.getLowerBound() + interval.getUpperBound()) / 2,
+                ((interval.getUpperBound() - interval.getLowerBound()) / 2) / 1.645);
+    }
+
+    public static LogNormalRandomVariable logNormalRandomWithInterval(Interval interval) {
+        return new LogNormalRandomVariable(interval.getLowerBound(), interval.getUpperBound());
+    }
+}
--- projects/monte-carlo/MonteCarlo.java
+++ projects/monte-carlo/MonteCarlo.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 
@@ -11,37 +12,74 @@ public class MonteCarlo {
 
         int min = 0;
         int max = 100;
-        UniformRandomVariable uniformRandomVariable = new UniformRandomVariable(min, max);
-        ArrayList<Double> uniformList = new ArrayList<>();
-        for (int i = 0; i < times; i++) uniformList.add(uniformRandomVariable.next());
-        System.out.println("uniform mean: " + uniformList.stream().collect(Collectors.averagingDouble(x -> x)));
+        var uniformRandom = new UniformRandomVariable(min, max);
+        var uniformList = new ArrayList<Double>();
+        for (int i = 0; i < times; i++) uniformList.add(uniformRandom.next());
+        System.out.println("uniform actual mean: " + uniformList.stream().collect(Collectors.averagingDouble(x -> x)));
         System.out.println("uniform expected mean: " + ((min + max) / 2));
-        System.out.println("uniform standard deviation: " + (Math.pow((max - min), 2) / 12));
         System.out.println();
 
         int normalMean = 7;
         int normalStdev = 2;
-        NormalRandomVariable normalRandomVariable = new NormalRandomVariable(normalMean, normalStdev);
-        ArrayList<Double> normalList = new ArrayList<>();
-        for (int i = 0; i < times; i++) normalList.add(normalRandomVariable.next());
+        var normalRandom = new NormalRandomVariable(normalMean, normalStdev);
+        var normalList = new ArrayList<Double>();
+        for (int i = 0; i < times; i++) normalList.add(normalRandom.next());
         var normalActualMean = normalList.stream().collect(Collectors.averagingDouble(x -> x));
         var normalActualStdev = Math.sqrt(normalList.stream().collect(Collectors.summingDouble(x -> Math.pow(x - normalActualMean, 2))) / times);
-        System.out.println("normal mean: " + normalActualMean);
+        System.out.println("normal actual mean: " + normalActualMean);
         System.out.println("normal expected mean: " + normalMean);
-        System.out.println("normal standard deviation: " + normalActualStdev);
+        System.out.println("normal actual standard deviation: " + normalActualStdev);
         System.out.println("normal expected standard deviation: " + normalStdev);
         System.out.println();
 
         int logMean = 5;
         int logStdev = 1;
-        LogNormalRandomVariable logNormalRandomVariable = new LogNormalRandomVariable(logMean, logStdev);
-        ArrayList<Double> logList = new ArrayList<>();
-        for (int i = 0; i < times; i++) logList.add(logNormalRandomVariable.next());
+        var logRandom = new LogNormalRandomVariable(logMean, logStdev);
+        var logList = new ArrayList<Double>();
+        for (int i = 0; i < times; i++) logList.add(logRandom.next());
         var logActualMean = logList.stream().collect(Collectors.averagingDouble(x -> Math.log(x)));
         var logActualStdev = Math.sqrt(logList.stream().collect(Collectors.summingDouble(x -> Math.pow(Math.log(x) - logActualMean, 2))) / times);
-        System.out.println("log mean: " + logActualMean);
+        System.out.println("log actual mean: " + logActualMean);
         System.out.println("log expected mean: " + logMean);
-        System.out.println("log standard deviation: " + logActualStdev);
+        System.out.println("log actual standard deviation: " + logActualStdev);
         System.out.println("log expected standard deviation: " + logStdev);
+        System.out.println();
+
+        int lowerBound = 5;
+        int upperBound = 95;
+        var uniformInterval = new Interval(lowerBound, upperBound);
+        var uniformRandomInterval = Interval.uniformRandomWithInterval(uniformInterval);
+        var uniformRandomIntervalList = new ArrayList<Double>();
+        for (int i = 0; i < times; i++) uniformRandomIntervalList.add(uniformRandomInterval.next());
+        uniformRandomIntervalList.sort(Comparator.comparingDouble(x -> x));
+        var uniformFivePercent = (int) (times * 0.05);
+        var uniformNinetyIntervalStart = uniformRandomIntervalList.get(uniformFivePercent);
+        var uniformNinetyIntervalEnd = uniformRandomIntervalList.get(times - uniformFivePercent);
+        System.out.println("uniform actual 90% interval lower bound: " + uniformNinetyIntervalStart + ", upper bound: " + uniformNinetyIntervalEnd);
+        System.out.println("uniform expected 90% interval " + uniformInterval.toString());
+        System.out.println();
+
+        var normalInterval = new Interval(lowerBound, upperBound);
+        var normalRandomInterval = Interval.normalRandomWithInterval(normalInterval);
+        var normalRandomIntervalList = new ArrayList<Double>();
+        for (int i = 0; i < times; i++) normalRandomIntervalList.add(normalRandomInterval.next());
+        normalRandomIntervalList.sort(Comparator.comparingDouble(x -> x));
+        var normalFivePercent = (int) (times * 0.05);
+        var normalNinetyIntervalStart = normalRandomIntervalList.get(normalFivePercent);
+        var normalNinetyIntervalEnd = normalRandomIntervalList.get(times - normalFivePercent);
+        System.out.println("normal actual 90% interval lower bound: " + normalNinetyIntervalStart + ", upper bound: " + normalNinetyIntervalEnd);
+        System.out.println("normal expected 90% interval " + normalInterval.toString());
+        System.out.println();
+
+        var logInterval = new Interval(lowerBound, upperBound);
+        var logRandomInterval = Interval.normalRandomWithInterval(logInterval);
+        var logRandomIntervalList = new ArrayList<Double>();
+        for (int i = 0; i < times; i++) logRandomIntervalList.add(logRandomInterval.next());
+        logRandomIntervalList.sort(Comparator.comparingDouble(x -> x));
+        var logFivePercent = (int) (times * 0.05);
+        var logNinetyIntervalStart = logRandomIntervalList.get(logFivePercent);
+        var logNinetyIntervalEnd = logRandomIntervalList.get(times - logFivePercent);
+        System.out.println("log actual 90% interval lower bound: " + logNinetyIntervalStart + ", upper bound: " + logNinetyIntervalEnd);
+        System.out.println("log expected 90% interval " + normalInterval.toString());
     }
 }
