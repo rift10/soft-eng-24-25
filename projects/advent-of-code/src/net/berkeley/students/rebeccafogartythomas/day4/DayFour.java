c29eb958e7fea3ca19ffb8ae5650f60791e49315
Rebecca <119640842+rift10@users.noreply.github.com>
2024-12-08 08:33:10 +0000

    day 4 part 2

--- projects/advent-of-code/src/net/berkeley/students/rebeccafogartythomas/day4/DayFour.java
+++ projects/advent-of-code/src/net/berkeley/students/rebeccafogartythomas/day4/DayFour.java
@@ -16,8 +16,7 @@ public class DayFour implements Day {
     @Override
     public void run() {
         list = Util.readFileToList(filePath);
-        findXMases();
-
+        findMasesInX();
         System.out.println(xmasCount);
     }
 
@@ -59,96 +58,63 @@ public class DayFour implements Day {
     }
 
     private boolean findSouthEast(int i, int j) {
-        // System.out.println("testing: " + i + ", " + j);
         if (i >= list.size() - (xmasLength + 1) || j >= list.get(i).length() - (xmasLength + 1)) return false;
-        // System.out.println(list.get(i).charAt(j));
-        // System.out.println(list.get(i + 1).charAt(j + 1));
-        // System.out.println(list.get(i + 2).charAt(j + 2));
-        // System.out.println(list.get(i + 3).charAt(j + 3));
         return list.get(i).charAt(j) == 'X' && list.get(i + 1).charAt(j + 1) == 'M' && list.get(i + 2).charAt(j + 2) == 'A' && list.get(i + 3).charAt(j + 3) == 'S';
     }
     
-    private void findXmases() {
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < list.get(i).length(); j++) {
-                if (findWest(i, j)) {
-                    // System.out.println("west x at: " + i + ", " + j); 
-                    xmasCount++;
-                }
-                if (findEast(i, j)) {
-                    // System.out.println("east x at: " + i + ", " + j); 
-                    xmasCount++;
-                }
-                if (findNorth(i, j)) {
-                    // System.out.println("north x at: " + i + ", " + j); 
-                    xmasCount++;
-                }
-                if (findSouth(i, j)) {
-                    // System.out.println("south x at: " + i + ", " + j); 
-                    xmasCount++;
-                }
-                if (findNorthWest(i, j)) {
-                    // System.out.println("northWest x at: " + i + ", " + j); 
-                    xmasCount++;
-                }
-                if (findNorthEast(i, j)) {
-                    // System.out.println("northEast x at: " + i + ", " + j); 
-                    xmasCount++;
-                }
-                if (findSouthWest(i, j)) {
-                    // System.out.println("southWest x at: " + i + ", " + j); 
-                    xmasCount++;
-                }
-                if (findSouthEast(i, j)) {
-                    // System.out.println("southEast x at: " + i + ", " + j); 
-                    xmasCount++;
-                }
-            }
-        }
-    }
-
-    private void findXMases() {
-        // 1191 is too low
-        // 2163 is too high
-        for (int i = 1; i < list.size() - 1; i++) {
-            for (int j = 1; j < list.get(i).length() - 1; j++) {
-                if (findMasMas(i, j)) {
-                    System.out.println("mas mas a at: " + i + ", " + j);
-                    xmasCount++;
-                }
-                if (findMasSam(i, j)) {
-                    System.out.println("mas sam a at: " + i + ", " + j);
-                    xmasCount++;
-                }
-                if (findSamMas(i, j)) {
-                    System.out.println("sam mas a at: " + i + ", " + j);
-                    xmasCount++;
-                }
-                if (findSamSam(i, j)) {
-                    System.out.println("sam sam a at: " + i + ", " + j);
-                    xmasCount++;
-                }
-            }
-        }
-    }
-
     private boolean findMasMas(int i, int j) {
-        return list.get(i).charAt(j) == 'A' && list.get(i - 1).charAt(j - 1) == 'M' && list.get(i + 1).charAt(j + 1) == 'S'
-                && ((list.get(i + 1).charAt(j - 1) == 'M') && (list.get(i - 1).charAt(j + 1) == 'S'));
+        return list.get(i).charAt(j) == 'A' && findMas_(i, j) && find_Mas(i, j);
     }
 
     private boolean findMasSam(int i, int j) {
-        return list.get(i).charAt(j) == 'A' && list.get(i - 1).charAt(j - 1) == 'M' && list.get(i + 1).charAt(j - 1) == 'S'
-                && ((list.get(i + 1).charAt(j - 1) == 'S') && (list.get(i - 1).charAt(j + 1) == 'M'));
+        return list.get(i).charAt(j) == 'A' && findMas_(i, j) && find_Sam(i, j);
     }
 
     private boolean findSamMas(int i, int j) {
-        return list.get(i).charAt(j) == 'A' && list.get(i - 1).charAt(j - 1) == 'S' && list.get(i + 1).charAt(j - 1) == 'M'
-                && ((list.get(i + 1).charAt(j - 1) == 'M') && (list.get(i - 1).charAt(j + 1) == 'S'));
+        return list.get(i).charAt(j) == 'A' && findSam_(i, j) && find_Mas(i, j);
     }
 
     private boolean findSamSam(int i, int j) {
-        return list.get(i).charAt(j) == 'A' && list.get(i - 1).charAt(j - 1) == 'S' && list.get(i + 1).charAt(j + 1) == 'M'
-                && ((list.get(i + 1).charAt(j - 1) == 'S') && (list.get(i + 1).charAt(j + 1) == 'M'));
+        return list.get(i).charAt(j) == 'A' && findSam_(i, j) && find_Sam(i, j);
     }
+
+    private boolean findMas_(int i, int j) {
+        return list.get(i - 1).charAt(j - 1) == 'M' && list.get(i + 1).charAt(j + 1) == 'S';
+    }
+
+    private boolean find_Mas(int i, int j) {
+        return (list.get(i + 1).charAt(j - 1) == 'M') && (list.get(i - 1).charAt(j + 1) == 'S');
+    }
+
+    private boolean findSam_(int i, int j) {
+        return list.get(i - 1).charAt(j - 1) == 'S' && list.get(i + 1).charAt(j + 1) == 'M';
+    }
+
+    private boolean find_Sam(int i, int j) {
+        return (list.get(i + 1).charAt(j - 1) == 'S') && (list.get(i - 1).charAt(j + 1) == 'M');
+    }
+
+    private void findXmases() {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).length(); j++) {
+                if (findWest(i, j)) xmasCount++;
+                if (findEast(i, j)) xmasCount++;
+                if (findNorth(i, j)) xmasCount++;
+                if (findSouth(i, j)) xmasCount++;
+                if (findNorthWest(i, j)) xmasCount++;
+                if (findNorthEast(i, j)) xmasCount++;
+                if (findSouthWest(i, j)) xmasCount++;
+                if (findSouthEast(i, j)) xmasCount++;
+            }
+        }
+    }
+
+    private void findMasesInX() {
+        for (int i = 1; i < list.size() - 1; i++) {
+            for (int j = 1; j < list.get(i).length() - 1; j++) {
+                if (findMasMas(i, j) || findMasSam(i, j) || findSamMas(i, j) || findSamSam(i, j)) xmasCount++;
+            }
+        }
+    }
+
 }
