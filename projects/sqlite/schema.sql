fff2ebe8a07a943aec279dcb5889a9902669912e
Rebecca <119640842+rift10@users.noreply.github.com>
2025-02-13 23:33:47 +0000

    start on sql stuff

---
+++ projects/sqlite/schema.sql
@@ -0,0 +1,3 @@
+create table if not exists classes (id string, name string, room string, period integer, teacher string);
+insert into classes values ("AS46Y", "Software Engineering", "H204", 6, "Mr. Seibel");
+select * from classes;
